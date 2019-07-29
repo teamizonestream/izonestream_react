e62d941f223ce9994db1b0f444c729efaa090328
Kang Chan <kentakang@hycoding.io>
2019-07-29 20:25:07 +0900

    Main 페이지 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Page, Header, Content, Title } from "../components/Page";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <Page>
+      <Header>
+        <Title>동영상 제목</Title>
+      </Header>
+      <Content />
+    </Page>
+  );
 };
 
 export default Main;

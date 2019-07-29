e5f6b9b5c6a2a335a321ce8c69719cee062f1054
Kang Chan <kentakang@hycoding.io>
2019-07-29 11:02:42 +0900

    GlobalStyle 추가
    index.js에 GlobalStyle 추가

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 import App from "./App";
+import GlobalStyle from "./components/globalStyle";
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
+    <GlobalStyle />
   </BrowserRouter>,
   document.getElementById("root")
 );

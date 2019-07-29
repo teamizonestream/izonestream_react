574746e2951566bd744f0948ce9c94a8446b44f1
Kang Chan <kentakang@hycoding.io>
2019-07-29 20:24:52 +0900

    globalStyle 폰트 종류 추가

--- src/components/globalStyle.js
+++ src/components/globalStyle.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 const globalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap');
+  @import url('https://fonts.googleapis.com/css?family=Noto+Sans+KR:400,500,700&display=swap&subset=korean');
   ${reset};
   html, body, #miyawaki_sakura {
     width: 100%;

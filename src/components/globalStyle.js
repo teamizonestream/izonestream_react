3df32dfe0b0f55a90f26b31ddcb601fabc124c16
Kang Chan <kentakang@hycoding.io>
2019-07-29 22:55:08 +0900

    Ionicons 추가

--- src/components/globalStyle.js
+++ src/components/globalStyle.js
@@ -3,6 +3,7 @@ import reset from "styled-reset";
 
 const globalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Noto+Sans+KR:400,500,700&display=swap&subset=korean');
+  @import url('https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css');
   ${reset};
   html, body, #miyawaki_sakura {
     width: 100%;

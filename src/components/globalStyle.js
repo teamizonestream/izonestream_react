d617969b9df79da3104580ed290c08cf5e658429
Kang Chan <kentakang@hycoding.io>
2019-07-29 10:44:27 +0900

    globalStyle 수정
    html, body, #root에 관한 스타일 추가

--- src/components/globalStyle.js
+++ src/components/globalStyle.js
@@ -3,6 +3,10 @@ import reset from "styled-reset";
 
 const globalStyles = createGlobalStyle`
   ${reset};
+  html, body, #root {
+    width: 100%;
+    height: 100%;
+  }
 `;
 
 export default globalStyles;
